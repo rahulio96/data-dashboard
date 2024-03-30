9b086d3512466decc65a0dfb30cc647f2dbee0f6
rahulio96 <rahgupta96@gmail.com>
2024-03-30 12:52:07 -0700

    Added header component and color changes

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import './header.css'
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>ComicDash</h1>
+            <h3>Made using the Marvel API!</h3>
+        </div>
+    )
+}
+
+export default Header
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,13 @@
 import '../../App.css'
 import ComicList from '../comic-list/ComicList'
+import Header from '../header/Header'
 
 function Home() {
 
   return (
     <>
-      <ComicList />
+        <Header />
+        <ComicList />
     </>
   )
 }
