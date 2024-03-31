290e52a67c16403e1bae727771b9b5f5efdb673e
rahulio96 <rahgupta96@gmail.com>
2024-03-30 17:54:22 -0700

    Added VERY basic slider

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -3,14 +3,16 @@ import './filter.css'
 function Filter() {
     return (
         <div className='filter-container'>
-            <input className='search' type="text" placeholder='Search...' />
+            <input className='search' type="text" placeholder='Search by name...' />
+
+            <div className='checkbox-container'>
+                Max Pages
+                <input className='slider' type="range" min="1" max="1000" value="500" />
+            </div>
 
             <div className='checkbox-container'>
                 Spider-Man
                 <input className='checkbox search' type="checkbox" />
-            </div>
-
-            <div className='checkbox-container'>
                 X-Men
                 <input className='checkbox' type="checkbox" />
             </div>
