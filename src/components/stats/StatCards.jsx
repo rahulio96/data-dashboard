8b913cf8763a84690b168c70177c962ee4cf3d34
rahulio96 <rahgupta96@gmail.com>
2024-03-31 14:25:22 -0700

    CSS module changes for stats

--- src/components/stats/StatCards.jsx
+++ src/components/stats/StatCards.jsx
@@ -1,4 +1,4 @@
-import './stats.css'
+import statCardsCSS from './StatCards.module.css'
 import { useEffect, useState } from 'react'
 
 function StatCards({ staticList }) {
@@ -13,36 +13,32 @@ function StatCards({ staticList }) {
             setLength(staticList.length)
             
             let totalPageCount = 0
-
             let min = Infinity
             let max = -Infinity
 
             for (let i = 0; i < staticList.length; i++) {
-                let obj = staticList[i]
-                
-                totalPageCount += obj.pageCount
-
+                let obj = staticList[i]       
                 let price = obj.prices[0].price
+
                 if (price > max) {
                     max = price
                 } else if (price < min) {
                     min = price
                 }
+                totalPageCount += obj.pageCount
             }
-
             setMinPrice(min)
             setMaxPrice(max)
             setAvgPage(totalPageCount / staticList.length)
-
         }
         calculateStats()
     }, [])
 
     return (
-        <div className="stat-container">
-            <div className='stat-card'>Average Page Count: <b>{avgPage}</b></div>
-            <div className='stat-card'>Total Entries: <b>{length}</b></div>
-            <div className='stat-card'>Price Range: <b>${minPrice} - ${maxPrice}</b></div>
+        <div className={statCardsCSS.container}>
+            <div className={statCardsCSS.card}>Average Page Count: <b>{avgPage}</b></div>
+            <div className={statCardsCSS.card}>Total Entries: <b>{length}</b></div>
+            <div className={statCardsCSS.card}>Price Range: <b>${minPrice} - ${maxPrice}</b></div>
         </div>
     )
 }
